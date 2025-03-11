cf8968918237af6cb8bdc8d03877600dc0041741
Milan Pjevic <milanpjevic@icloud.com>
2025-03-11 17:38:33 +0100

    Pot: rename scss classes

--- src/ui/Pot/Pot.jsx
+++ src/ui/Pot/Pot.jsx
@@ -1,7 +1,6 @@
 /** @format */
 
 import { formatToDollars } from "../../utils/helpers";
-
 import ButtonDots from "../ButtonDots/ButtonDots";
 import ButtonPot from "../ButtonPot/ButtonPot";
 
@@ -14,25 +13,25 @@ function Pot({ data }) {
     <div className={styles.pot}>
       <div className={styles.pot__header}>
         <div
-          className={styles["pot__header-circle"]}
+          className={styles.pot__icon}
           style={{ backgroundColor: data.theme }}
         ></div>
-        <div className={styles.pot__heading}>{data.name}</div>
+        <div className={styles.pot__title}>{data.name}</div>
         <ButtonDots />
       </div>
 
-      <div className={styles.pot__data}>
-        <div className={styles["pot__data--saved"]}>
-          <p className={styles["pot__data--saved-text"]}>Total Saved</p>
-          <div className={styles["pot__data--saved-value"]}>
+      <div className={styles.pot__content}>
+        <div className={styles.saved}>
+          <p className={styles.saved__label}>Total Saved</p>
+          <div className={styles.saved__value}>
             {formatToDollars(data.total)}
           </div>
         </div>
 
-        <div className={styles["pot__data--target"]}>
-          <div className={styles["pot__data--target-range"]}>
+        <div className={styles.target}>
+          <div className={styles.target__progress}>
             <div
-              className={styles.range__value}
+              className={styles.target__bar}
               style={{
                 width: `${percentage}%`,
                 backgroundColor: data.theme,
@@ -40,18 +39,18 @@ function Pot({ data }) {
             ></div>
           </div>
 
-          <div className={styles.range__box}>
-            <div className={styles.range__percentage}>
+          <div className={styles.target__info}>
+            <div className={styles.target__percentage}>
               {percentage.toFixed(2)}%
             </div>
-            <div className={styles.range__target}>
+            <div className={styles.target__amount}>
               Target of {formatToDollars(data.target)}
             </div>
           </div>
         </div>
       </div>
 
-      <div className={styles.pot__options}>
+      <div className={styles.pot__actions}>
         <ButtonPot>+ Add Money</ButtonPot>
         <ButtonPot>Withdraw</ButtonPot>
       </div>
