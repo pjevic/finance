b9a564e04fb3557cb60ea7596e3452cb477843b2
Milan Pjevic <milanpjevic@icloud.com>
2025-03-12 13:32:11 +0100

    Recurring Bils: draft

--- src/pages/RecurringBills/RecurringBills.jsx
+++ src/pages/RecurringBills/RecurringBills.jsx
@@ -1,7 +1,26 @@
 /** @format */
 
+import PageHeader from "../../ui/PageHeader/PageHeader";
+import Heading from "../../ui/Heading/Heading";
+import Spinner from "../../ui/Spinner/Spinner";
+
+import styles from "./RecurringBills.module.scss";
+
 function RecurringBills() {
-  return <>Recurring Bills</>;
+  return (
+    <div className={styles.bills}>
+      <PageHeader>
+        <Heading>Recurring Bills</Heading>
+      </PageHeader>
+
+      <div className={styles.bills__body}>
+        <div className={styles.bills__total}>TOTAL</div>
+        <div className={styles.bills__summary}>SUMMARY</div>
+        <div className={styles.bills__table}>TABLE</div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default RecurringBills;
